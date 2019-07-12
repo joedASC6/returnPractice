ec1559852f85ad62fa22294c75ab6a712aff98a4
joedASC6 <jd113q@bhsec.bard.edu>
2019-07-12 09:47:42 -0400

    creates function with return value

---
+++ returnPractice.js
@@ -0,0 +1,12 @@
+//by default all teh js funtions return to a value
+//if the return value is not specified the function returns undefined 
+
+function someFunc() {
+//return syntax
+return "Hello World";
+//return 5; (nothing under return will not be said)
+}
+
+const result = someFunc();
+ 
+console.log (result);
